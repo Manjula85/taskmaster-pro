52fa4298ce63edd5d738ef0614839c592b89e624
Manjula <mguneratne3@gmail.com>
2021-10-06 16:35:16 -0400

    icons and responsiveness added -5.6 done

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,12 +54,11 @@ var auditTask = function(taskEl) {
     .text()
     .trim();
 
-  console.log(date);
-
   // convert to moment object at 5:00pm
   var time = moment(date, "L").set("hour", 17);
 
-  console.log(time);
+  //testing output
+  console.log(taskEl);
 
   // remove any old classes from element
   $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
@@ -77,7 +76,7 @@ setInterval(function() {
   $(".card .list-group-item").each(function(index,el){
     auditTask(el);
   });
-}, 5000);
+}, (1000 * 60) * 30);
 
 //**************************************************** */
 
